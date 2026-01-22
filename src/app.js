035bf38290a78c7f756375e26b3fa07554107bca
erikaportes <152933693+erikaportes@users.noreply.github.com>
2026-01-22 19:15:53 +0000

    Cambios

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ import "./style.css";
 
 window.onload = function () {
   //write your code here
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let who = ['Mi perro', 'Mi abuela', 'El cartero', 'El pájaro'];
+  let action = ['se comió', 'hizo trizas', 'aplastó', 'rompió'];
+  let what = ['mi tarea', 'mi teléfono', 'el automóvil', 'la computadora'];
+  let when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras ejercitaba', 'durante mi almuerzo', 'mientras estaba rezando'];
 
   let excuse = who[Math.floor(Math.random() * who.length) ] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector("#excuse").innerHTML = excuse;
   
   // let parrafo = document.getElementById("excuse");  
   // parrafo.innerHTML = excuse;
-  document.getElementById("excuse").textContent = excuse;
+
+  // document.getElementById("excuse").textContent = excuse;
 };
